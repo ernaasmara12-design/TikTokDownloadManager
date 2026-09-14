d0f3d641eaeb56528fb88388ada8ce0fea913ea8
ernaasmara12-design <erna.asmara12@gmail.com>
2026-09-14 09:14:06 +0700

    Update MainActivity.java

--- app/src/main/java/com/example/tiktokdownloadmanager/MainActivity.java
+++ app/src/main/java/com/example/tiktokdownloadmanager/MainActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
-import android.os.Build;
 import android.view.Gravity;
 import android.view.View;
 import android.webkit.CookieManager;
